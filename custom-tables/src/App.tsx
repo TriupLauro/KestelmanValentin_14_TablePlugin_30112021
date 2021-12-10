fefaf4eac332114ed6941c5fef50c5e1a0bd72c3
Valentin Kestelman <valaourok@gmail.com>
2021-12-10 09:00:56 +0100

    Added Images to the Readme

--- custom-tables/src/App.tsx
+++ custom-tables/src/App.tsx
@@ -1,21 +1,23 @@
 import CustomTable from "./Components/CustomTable";
-import {columnsLabels, generatedData} from "./data/data";
+import {columnsLabels, contentData, generatedData} from "./data/data";
 import {bigColumns, bigRandomNumber} from "./data/bigData";
 import React from "react";
 
 function App() {
 
-  return (
-      <>
-        <CustomTable
-            columns={columnsLabels} data={generatedData}
-        />
-        <CustomTable
-          columns={bigColumns} data={bigRandomNumber}
-        />
-      </>
-
-  )
+    return (
+        <>
+            <CustomTable
+                data={contentData} columns={columnsLabels}
+            />
+            <CustomTable
+                columns={columnsLabels} data={generatedData}
+            />
+            <CustomTable
+                columns={bigColumns} data={bigRandomNumber}
+            />
+        </>
+    )
 }
 
 export default App
